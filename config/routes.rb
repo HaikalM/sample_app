e4d90baa683003c8db70c30a1bc99ce88c54c504
HaikalM <maulana.haikal.98@gmail.com>
2016-11-01 08:42:08 +0800

    change route and link in page layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   
-  get '/home', to: 'static_pages#home', as: 'home'
   get '/help', to: 'static_pages#help', as: 'help'
   get '/about', to: 'static_pages#about', as: 'about'
   get '/contact', to: 'static_pages#contact', as: 'contact'
