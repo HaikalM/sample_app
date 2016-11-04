bcea9654f33ac8a969471049f5291ab9da74b91f
HaikalM <maulana.haikal.98@gmail.com>
2016-11-04 14:08:26 +0800

    validty user email at models_test user_test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,9 +6,21 @@ class UserTest < ActiveSupport::TestCase
   # end
   def setup
     @user = User.new(name: "Example User", email: "user@example.com")
+    @u1 = User.new(name: " ", email: "tst@gmail.com")
   end
 
   test "should be valid" do
     assert @user.valid?
+    assert_not @u1.valid?
+  end
+
+  test "Name should be present" do
+  	@user.name = "   "
+  	assert_not @user.valid?
+  end
+
+  test "Email should be present" do
+  	@user.email = "   "
+  	assert_not @user.valid?
   end
 end
