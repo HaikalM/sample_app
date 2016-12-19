2663e5c0d9da80142aa393d5cd239977ac1d7944
HaikalM <maulana.haikal.98@gmail.com>
2016-12-19 09:11:34 +0800

    actionmailer base to environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'maulanahaikal',
+  :password => 'rafli007',
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
